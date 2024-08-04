bd65bb842dcb9bf4d4dbcc672c739e60b920028c
idontgiveaf <156725392+1dontgiveaf@users.noreply.github.com>
2024-08-04 11:53:51 -0400

    Create user.js

---
+++ user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name         Linkvertise Bypasser
+// @match        *://linkvertise.com/?*
+// @version      1.0
+// @description  Bypasses linkvertise links using a API.
+// @author       .idontgiveaf
+// @grant        GM_notification
+// ==/UserScript==
