74c3801df822332e512c4f8d1c3a78554301ce88
idontgiveaf <156725392+1dontgiveaf@users.noreply.github.com>
2024-08-04 11:58:16 -0400

    Update meta.js

--- meta.js
+++ meta.js
@@ -5,4 +5,7 @@
 // @description  Bypasses linkvertise links using a API.
 // @author       .idontgiveaf
 // @grant        GM_notification
+// @namespace   https://github.com/1dontgiveaf/Other-Bypassers
+// @downloadURL https://github.com/1dontgiveaf/Other-Bypassers/raw/main/user.js
+// @updateURL   https://github.com/1dontgiveaf/Other-Bypassers/raw/main/meta.js
 // ==/UserScript==
